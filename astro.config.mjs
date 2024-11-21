b9c2b31ffac03b2f0babdc256569d804cc558059
thoroughmas <9094824+thoroughmas@users.noreply.github.com>
2024-11-21 23:36:08 +1030

    Update files now

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { autoNewTabExternalLinks } from './src/autoNewTabExternalLinks';
-
 import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thomid.me',
