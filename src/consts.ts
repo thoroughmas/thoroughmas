f2e7baf739bddc05157ea8ac77130c9a6d978b6d
Thoroughmas <thoroughmas@users.noreply.github.com>
2026-04-11 13:58:13 +0930

    v2.13

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = 'Thomas Midena';
-export const SITE_TAGLINE = 'Generally genuinely enthusiastic';
-export const SITE_DESCRIPTION = 'Generally genuinely enthusiastic';
+export const SITE_TAGLINE = '';
+export const SITE_DESCRIPTION =
+	'Thomas is a theatremaker and filmmaker with a passion for ethical technology. This website is a portal to all of his projects.';
